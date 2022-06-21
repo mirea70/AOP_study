a1810a782854aa0fb5eb2e6d8cc3f2bcfb8df58b
GrowMyself <deae670@daum.net>
2022-06-21 16:56:04 +0900

    Finished the study

---
+++ src/main/java/start/aop/order/aop/Aspect2.java
@@ -0,0 +1,20 @@
+package start.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class Aspect2 {
+    @Pointcut("execution(* start.aop.order..*(..))")
+    private void allOrder(){}
+
+    @Around("allOrder()")
+    public Object logging(ProceedingJoinPoint joinpoint) throws Throwable {
+        log.info("log -> {}", joinpoint.getSignature());
+        return joinpoint.proceed();
+    }
+}
